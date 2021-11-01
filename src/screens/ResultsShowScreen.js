f6f2ea77d9c5eaad9024fc78f4fd4890276ddc7a
Matthew Denune <mattdenune@gmail.com>
2021-11-01 17:05:38 -0400

    added edgecases for no results

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, FlatList, Image } from 'react-native';
 
 import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
 	const [result, setResult] = useState(null)
 	const id = navigation.getParam('id');
-  console.log("🚀 ~ file: ResultsShowScreen.js ~ line 8 ~ ResultsShowScreen ~ result", result)
 
 	const getResult = async (id) => {
 		const response = await yelp.get(`/${id}`);
@@ -17,15 +16,31 @@ const ResultsShowScreen = ({ navigation }) => {
 		getResult(id);
 	}, [])
 
+	if (!result) {
+		return null;
+	}
+
 	return (
 		<View>
-			<Text>Results Show</Text>
+			<Text>{result.name}</Text>
+			<FlatList
+				data={result.photos}
+				keyExtractor={(photo) => photo}
+				renderItem={({ item }) => {
+					return <Image style={styles.image} source={{uri: item}}/>
+				}}
+			/>
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
-
+	image: {
+		width: 250,
+		height: 150,
+		margin: 15,
+		borderRadius: 5,
+	}
 })
 
 export default ResultsShowScreen;
